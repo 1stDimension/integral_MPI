524848e278d04c9ea14155c6b702262defd6dcfc
1stDimension <23144389+1stDimension@users.noreply.github.com>
2020-04-05 01:17:10 +0200

    Basic main for testing purpouses

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "integrals.h"
+
 int main(int argc, char ** argv){
+
+  
+  double result = integrate(linear, 0,100, 5);
+
+  printf("Integral is %g\n", result);
 
   return 0;
 }
