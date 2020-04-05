abff6d66fd06d4063b6f1fe9ba337c0c159169df
1stDimension <23144389+1stDimension@users.noreply.github.com>
2020-04-05 13:36:38 +0200

    adding debug printfs

--- main.c
+++ main.c
@@ -5,9 +5,11 @@
 
 int main(int argc, char ** argv){
 
-  
-  double result = integrate(linear, 0,100, 5);
-
+  double (*func_ptr)(double) = &linear;
+  double begin = 0, end = 100;
+  double result = integrate(func_ptr, 0,100, 5);
+  printf("begin is %g end is %g \n", begin , end);
+  printf("f(begin) is %g f(end) is %g \n", (*func_ptr)(begin) , (*func_ptr)(end));
   printf("Integral is %g\n", result);
 
   return 0;
