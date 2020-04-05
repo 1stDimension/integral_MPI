a3a7e4cdf22846fde6784973df1988c95b4ed651
1stDimension <23144389+1stDimension@users.noreply.github.com>
2020-04-05 13:37:01 +0200

    implementing integrate function

--- integrals.c
+++ integrals.c
@@ -3,8 +3,17 @@
 #include <stdio.h>
 
 double integrate(double (*func)(double), double begin, double end, int num_points){
-  printf("Not implemented");
-  return 0;
+  if (num_points < 2){
+    printf("Too few point to calculate");
+    return 0;
+  }
+  double step = (end - begin) / (double) (num_points - 1);
+  double integral  = 0.0;
+  for(int i = 0 ; i < num_points -1 ; i++){
+     double tmp = (func(begin + i * step) + func(begin + (i+1) * step))/2.0 * step;
+     integral += tmp;
+  }
+  return integral;
 }
 
 double linear(double x){
