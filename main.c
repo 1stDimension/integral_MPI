2b4a40d092a5e021f2d0d349760786f0b1c7fcf8
1stDimension <23144389+1stDimension@users.noreply.github.com>
2020-04-05 18:01:27 +0200

    Commenting out non MPI code

--- main.c
+++ main.c
@@ -6,12 +6,12 @@
 
 int main(int argc, char ** argv){
 
-  double (*func_ptr)(double) = &linear;
-  double begin = 0, end = 100;
-  double result = integrate(func_ptr, 0,100, 5);
-  printf("begin is %g end is %g \n", begin , end);
-  printf("f(begin) is %g f(end) is %g \n", (*func_ptr)(begin) , (*func_ptr)(end));
-  printf("Integral is %g\n", result);
+  // double (*func_ptr)(double) = &linear;
+  // double begin = 0, end = 100;
+  // double result = integrate(func_ptr, 0,100, 5);
+  // printf("begin is %g end is %g \n", begin , end);
+  // printf("f(begin) is %g f(end) is %g \n", (*func_ptr)(begin) , (*func_ptr)(end));
+  // printf("Integral is %g\n", result);
 
   printf("MPI Environment begins\n");
   MPI_Init(NULL, NULL);
