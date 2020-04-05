1e52989a12eb05eba5f5dc246d5dabeee2aabfca
1stDimension <23144389+1stDimension@users.noreply.github.com>
2020-04-05 17:55:19 +0200

    Adding basic mpi

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <mpi.h>
 
 #include "integrals.h"
 
@@ -12,5 +13,25 @@ int main(int argc, char ** argv){
   printf("f(begin) is %g f(end) is %g \n", (*func_ptr)(begin) , (*func_ptr)(end));
   printf("Integral is %g\n", result);
 
+  printf("MPI Environment begins\n");
+  MPI_Init(NULL, NULL);
+
+  int world_size, rank;
+  //How many processes
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  //Position inside processes
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  if(rank == 0){
+    printf("I'm master\n");
+    // int slave_batch = ;
+  } else {
+    int my_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    printf("I'm slave nr %d\n", my_rank);
+  }
+
+  MPI_Finalize();
+  printf("MPI Environment end\n");
   return 0;
 }
