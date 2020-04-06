27561617bd72324eb5e7026967d1a886123c1514
1stDimension <23144389+1stDimension@users.noreply.github.com>
2020-04-06 21:27:27 +0200

    reformat

--- main.c
+++ main.c
@@ -4,7 +4,8 @@
 
 #include "integrals.h"
 
-int main(int argc, char ** argv){
+int main(int argc, char **argv)
+{
 
   // double (*func_ptr)(double) = &linear;
   // double begin = 0, end = 100;
@@ -21,11 +22,13 @@ int main(int argc, char ** argv){
   //Position inside processes
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  if(rank == 0){
+  if (rank == 0)
+  {
     printf("I'm master\n");
-    
+
     printf("World size = %d\n", world_size);
-    if (argc != 4){
+    if (argc != 4)
+    {
       printf("Not sufficient arguments");
       exit(EXIT_FAILURE);
     }
@@ -34,34 +37,36 @@ int main(int argc, char ** argv){
     int number_of_points = atoi(argv[3]);
 
     int number_of_regions = number_of_points - 1;
-    int slave_batch = number_of_points/world_size;
+    int slave_batch = number_of_points / world_size;
     int master_batch = number_of_points % world_size;
 
-      double m = world_size * (slave_batch + 1);
+    double m = world_size * (slave_batch + 1);
 
-      double step = (end - begin)/(world_size); 
-      printf("slave batch = %d\n", slave_batch);
-      printf("master batch = %d\n", master_batch);
-      printf("step = %g\n",step);
-      for(int i = 1; i < world_size; i++){
-        // send to i their begin and end and num points
-        double b,e;
-        int p;
-        b = begin + (i -1) * step;
-        e = begin + i * step;
-        p = slave_batch + 1;
-        printf("b = %g e = %g p = %d\n", b,e,p);
-      }
-      double m_b = begin + (world_size - 1) * step;
-      double m_e = end;
-      int m_p = master_batch + 1;
-      printf("m_b = %g m_e = %g m_p = %d\n", m_b, m_e, m_p);
+    double step = (end - begin) / (world_size);
+    printf("slave batch = %d\n", slave_batch);
+    printf("master batch = %d\n", master_batch);
+    printf("step = %g\n", step);
+    for (int i = 1; i < world_size; i++)
+    {
+      // send to i their begin and end and num points
+      double b, e;
+      int p;
+      b = begin + (i - 1) * step;
+      e = begin + i * step;
+      p = slave_batch + 1;
+      printf("b = %g e = %g p = %d\n", b, e, p);
     }
-    
-  } else {
-    // printf("I'm slave nr %d\n", rank);
+    double m_b = begin + (world_size - 1) * step;
+    double m_e = end;
+    int m_p = master_batch + 1;
+    printf("m_b = %g m_e = %g m_p = %d\n", m_b, m_e, m_p);
   }
+}
+else
+{
+  // printf("I'm slave nr %d\n", rank);
+}
 
-  MPI_Finalize();
-  return 0;
+MPI_Finalize();
+return 0;
 }
