25b6f45823c91b4f8169ab66cad40c8cbefa02b3
1stDimension <23144389+1stDimension@users.noreply.github.com>
2020-04-07 00:47:15 +0200

    removing commented code

--- main.c
+++ main.c
@@ -14,11 +14,6 @@ int main(int argc, char **argv)
 {
 
   double (*func_ptr)(double) = &linear;
-  // double begin = 0, end = 100;
-  // double result = integrate(func_ptr, 0,100, 5);
-  // printf("begin is %g end is %g \n", begin , end);
-  // printf("f(begin) is %g f(end) is %g \n", (*func_ptr)(begin) , (*func_ptr)(end));
-  // printf("Integral is %g\n", result);
 
   MPI_Init(&argc, &argv);
 
