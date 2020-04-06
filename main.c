b59e6ec126d28f4d0475a43f740b094e4eb7244e
1stDimension <23144389+1stDimension@users.noreply.github.com>
2020-04-07 00:31:24 +0200

    Function of witch integral will be calculated

--- main.c
+++ main.c
@@ -13,7 +13,7 @@
 int main(int argc, char **argv)
 {
 
-  // double (*func_ptr)(double) = &linear;
+  double (*func_ptr)(double) = &linear;
   // double begin = 0, end = 100;
   // double result = integrate(func_ptr, 0,100, 5);
   // printf("begin is %g end is %g \n", begin , end);
